5d551fb10d7ba5eb3567eb296ccd790bc4426344
Mark Raymond <markcharlesraymond@gmail.com>
2015-11-08 17:54:38 +0000

    Initial commit for main() code & basic e2e test

---
+++ main_test.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "os/exec"
+    "strings"
+    "testing"
+)
+
+// TODO Move duplicate code into its own setup function.
+// TODO Re-compile the go script under test before all tests run.
+
+func TestInvalidNumberOfArgs(t *testing.T) {
+    envVars := getEnvVarsAsMap()
+
+    scriptUnderTest := fmt.Sprintf("%s/bin/golang-weather-app",
+        envVars["GOPATH"])
+
+    cmd := exec.Command(scriptUnderTest, "foo")
+    stdout, stderr := cmd.CombinedOutput()
+
+    errMsg := getErrorTextFromOutput(stdout)
+
+    if string(errMsg) != "Invalid number of positional arguments: 2 required\n" {
+        t.Error(fmt.Sprintf("Message '%s' doesn't match expected error\n",
+            errMsg))
+    }
+
+    if stderr == nil {
+        t.Error("Script should fail with exit code 1")
+    }
+}
+
+func TestInvalidCountryCode(t *testing.T) {
+    envVars := getEnvVarsAsMap()
+
+    scriptUnderTest := fmt.Sprintf("%s/bin/golang-weather-app",
+        envVars["GOPATH"])
+
+    cmd := exec.Command(scriptUnderTest, "foo", "bar")
+    stdout, stderr := cmd.CombinedOutput()
+
+    errMsg := getErrorTextFromOutput(stdout)
+
+    if string(errMsg) != "Country code 'bar' is invalid\n" {
+        t.Error(fmt.Sprintf("Message '%s' doesn't match expected error\n",
+            errMsg))
+    }
+
+    if stderr == nil {
+        t.Error("Script should fail with exit code 1")
+    }
+}
+
+
+// The error message logged to stdout has the form:
+// [yyyy/mm/dd] [hh:mm:ss] [message]
+// We just want to check the message as the timestamp is variable.
+func getErrorTextFromOutput(stdout []byte) (errMsg string) {
+    errMsg = string(stdout)[20:]
+
+    return errMsg
+}
+
+// Return the key/values listed in ENV as a map.
+func getEnvVarsAsMap() (envVars map[string]string) {
+    envVars = map[string]string{}
+
+    for _, val := range os.Environ() {
+        keyValSlice := strings.Split(val,"=")
+
+        envVars[keyValSlice[0]] = keyValSlice[1]
+    }
+
+    return envVars
+}
